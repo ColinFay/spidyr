146f953f6b1c1615e6dcc77e293ca33f3f70b5b1
colin <colin@thinkr.fr>
2024-06-12 11:12:18 +0200

    feat: with initSpidyr

--- tests/library.test.js
+++ tests/library.test.js
@@ -1,22 +1,80 @@
 // jest tests for library.js
 const { WebR } = require('webr');
-const { Library } = require('../src/library.js');
-const webR = new WebR();
+const path = require('path');
+const {
+   LibraryFromPackage,
+   library,
+  mountLocalPackage
+} = require('../src/library.js');
 
-test('library works', async () => {
-  await webR.init();
+test('LibraryFromPackage works', async () => {
+  globalThis.spidyr_webR = new WebR();
+  await globalThis.spidyr_webR.init()
 
-  const lib = new Library('utils');
-  await lib.load(webR);
+  const lib = new LibraryFromPackage('utils');
+  await lib.load();
 
   expect(lib.lib).toBe('utils');
   expect(lib.namespace).toContain('head');
 
   expect(lib.head).toBeDefined();
 
-  const tools = new Library('tools');
-  await tools.load(webR);
+  const tools = new LibraryFromPackage('tools');
+  await tools.load();
   expect(tools.namespace).toContain('toTitleCase');
-  await webR.close();
+
+  await globalThis.spidyr_webR.close();
 
 });
+
+test('library works', async () => {
+  globalThis.spidyr_webR = new WebR();
+  await globalThis.spidyr_webR.init()
+
+  const lib = await library('utils');
+
+  expect(lib.lib).toBe('utils');
+  expect(lib.namespace).toContain('head');
+
+  expect(lib.head).toBeDefined();
+
+  const tools = new LibraryFromPackage('tools');
+  await tools.load();
+  expect(tools.namespace).toContain('toTitleCase');
+
+  await globalThis.spidyr_webR.close();
+
+});
+
+test('mountLocalPackage works', async () => {
+  var dns = require('dns')
+  has_internet = await dns.promises.lookup('google.com').then(
+    () => true
+  ).catch(() => false)
+  if (!has_internet) {
+    console.log("No internet connection, skipping test")
+    return
+  } else {
+    globalThis.spidyr_webR = new WebR();
+    await globalThis.spidyr_webR.init();
+    await globalThis.spidyr_webR.evalR("webr::install('pkgload', repos = 'https://repo.r-wasm.org/')");
+
+    await mountLocalPackage(
+      path.join(
+        __dirname,
+        'fixtures',
+        'rfuns'
+      )
+    )
+    const has_test_package = await globalThis.spidyr_webR.evalRBoolean(
+      '"package:rfuns" %in% search()'
+    )
+
+    expect(
+      has_test_package
+    ).toBe(true)
+
+    await globalThis.spidyr_webR.close();
+  }
+
+}, 100000);
---
+++ tests/utils.test.js
@@ -0,0 +1,43 @@
+// // jest tests for utils.js
+
+const { checkForSpidyrWebR, checkIsInstalled } = require('../src/utils.js');
+const { WebR } = require('webr');
+
+test('checkForSpidyrWebR works', async () => {
+
+  globalThis.spidyr_webR = undefined;
+
+  expect(
+    () => {
+      checkForSpidyrWebR()
+    }
+  ).toThrow("❌ Unable to find spidyr_webR.\nPlease run `initSpidyr()` first.");
+
+  globalThis.spidyr_webR = true;
+
+  console.log(checkForSpidyrWebR())
+
+  const res = checkForSpidyrWebR()
+
+  expect(res).toBe(true);
+
+})
+
+test("checkIsInstalled works", async () => {
+  globalThis.spidyr_webR = new WebR();
+  await globalThis.spidyr_webR.init()
+
+  const is_tools_installed = await checkIsInstalled(
+    "utils"
+  );
+  expect(is_tools_installed).toBe(true);
+
+
+  expect(
+    async () => {
+      await checkIsInstalled("blabla")
+    }
+  ).rejects.toThrow("blabla is required");
+
+
+})
